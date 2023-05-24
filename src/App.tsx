00567f5e9a3f203b81d3b1cc0307156da692633e
Ricardo Rolim de Oliveira <ricardorolim27@gmail.com>
2023-05-24 12:09:33 -0300

    add react-router-dom
    add react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { BrowserRouter } from "react-router-dom";
+import AppRoutes from "./routes";
 
 function App() {
   return (
-    <div className="App">
-      teste  
-    </div>
+    <BrowserRouter>
+      <AppRoutes/>
+    </BrowserRouter>
   );
 }
 
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,1 +1,14 @@
-export {};
+import { Routes, Route, Navigate } from "react-router-dom";
+
+function AppRoutes() {
+  return (
+    <>
+      <Routes>
+        <Route path="/index" element={<h1>Página Inicial</h1>} />
+        <Route path="*" element={<Navigate to="/index"/>}/>
+      </Routes>
+    </>
+  );
+}
+
+export default AppRoutes;
